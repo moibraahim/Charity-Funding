fc064d1a379089e660fb1f459387af49ee407403
YehiaM11 <93736300+YehiaM11@users.noreply.github.com>
2021-12-13 23:01:47 +0200

    loginjs
    Login Javascript

---
+++ login.js
@@ -0,0 +1,17 @@
+function onButtonClickvalid()
+{    
+  var mailflg=0;
+  var x=document.getElementById('email').value; 
+  for(i=0;i<x.length;i++)
+  {
+    if(x[i]=="@")
+    {
+        mailflg++;
+    }
+  }
+  if ( x[x.length -1]!="m" &&x[x.length -2]!="o"&&x[x.length -3]!="c" &&x[x.length -4]!="." || flga==0)
+  {
+      alert("please enter valid mail ")
+      return false;
+  }
+}
