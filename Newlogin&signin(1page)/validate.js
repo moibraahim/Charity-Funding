3f515446dca1c060e3cc1b28f0e04e62f1484831
YehiaM11 <93736300+YehiaM11@users.noreply.github.com>
2021-12-24 19:08:35 +0200

    Update validate.js
    modified the validation of inputs

--- Newlogin&signin(1page)/validate.js
+++ Newlogin&signin(1page)/validate.js
@@ -1,34 +1,36 @@
 function onButtonClickvalid() 
 {
-                var user = document.getElementById("username").value;                               
-                var eml=document.getElementById("mail").value;                               
-                var pass=document.getElementById("pswrd").value;                               
+                var flag=0;
+                var user = document.getElementById("username").value; 
+                var user2 = document.getElementById("username");                              
+                var eml=document.getElementById("mail").value;
+                var eml2 = document.getElementById("mail");                                   
+                var pass=document.getElementById("pswrd").value;                              
+                var pass2=document.getElementById("pswrd");                                
                 var letters = /^[A-Za-z]+$/;
                 var ml = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
                 var ps=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
-                if(pass.match(ps))
+                if(eml.match(ml) && pass.match(ps) && user.match(letters))
                 {
-                return true;
+                    return true;
                 }
-                else
-                {
-                alert("password must contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character");                
-                }  
-                if(eml.match(ml))
-                {
-                return true;
+                else{
+                        if(!eml.match(ml))
+                        {
+                            alert("mail must contain '@' and '.'");                            
+                            eml2.style.border = "red solid 3px";      
+                        }
+                        if(!user.match(ml))
+                        {
+                            alert("Username must have alphabet characters only");
+                            user2.innerHTML="Please enter a valid name";  
+                            user2.style.border = "red solid 3px";      
+                        }
+                        if(!pass.match(ml))
+                        {
+                            alert("password must contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character");
+                            pass2.style.border = "red solid 3px";      
+                        }
+                        return false
                 }
-                else
-                {
-                alert("mail must contain '@' and '.'");            
-                }    
-               if(user.match(letters))
-                {
-                    alert("yessss");
-                return true;
-                }
-                else
-                {
-                alert("Username must have alphabet characters only");
-                }    
  }
