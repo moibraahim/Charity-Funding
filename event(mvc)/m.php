d44845b0c33d86802da9f91e3edefc3409b4c47e
YehiaM11 <93736300+YehiaM11@users.noreply.github.com>
2022-01-24 06:17:55 +0200

    Update m.php

--- event(mvc)/m.php
+++ event(mvc)/m.php
@@ -1,39 +1,35 @@
 <?php
-class event
-{
-public $id;
-public $name;
-public $code;
-public $description;
-public $location;
-public $isdeleted;
-public $createdat;
-public $updatedat;
-function insert($name,$code,$description,$location)
+class event {
+    public $id;
+    public $name;
+    public $code;
+    public $description;
+    public $location;
+    public $isdeleted;
+    public $createdat;
+    public $updatedat;
+    function __construct($id)
+    {
+     $con = mysqli_connect('localhost','root','','charityfunding');
+     $sql="select * from event where id=$id";
+     $eventdataset = mysqli_query($con,$sql);
+     if($row =mysqli_fetch_array($eventdataset))
+     {
+         $this->name=$row["name"];
+         $this->code=$row["code"];
+         $this->description=$row["description"];
+         $this->location=$row["location"];
+         $this->isdeleted=$row["isdeleted"];
+         $this->createdat=$row["createdat"];
+         $this->updatedat=$row["updatedat"];
+     }
+    }
+    function insert($name,$code,$description,$location)
 {
     $con = mysqli_connect('localhost','root','','charityfunding');   
     $sql="INSERT INTO event( name, code, description, location) VALUES ('$name','$code','$description','$location')";
-    echo $sql;
+    //echo $sql;
     $eventdataset = mysqli_query($con,$sql);
 }
-function __construct($id)
-{
- $con = mysqli_connect('localhost','root','','charityfunding');
- $sql="select * from event where id=$id";
- $eventdataset = mysqli_query($con,$sql);
- if($row =mysqli_fetch_array($eventdataset))
- {
-     $this->name=$row["name"];
-     $this->code=$row["code"];
-     $this->description=$row["description"];
-     $this->location=$row["location"];
-     $this->isdeleted=$row["isdeleted"];
-     $this->createdat=$row["createdat"];
-     $this->updatedat=$row["updatedat"];
- }
+
 }
-}
-$eventob= new event(0);
-//$eventob->insert("charity lol","tysjsnw","collect ppl to....","luxor"); 
-//echo($eventob->name);
-?>
