3ec90e2e60585fa1309515b56d960297586efb1f
YehiaM11 <93736300+YehiaM11@users.noreply.github.com>
2022-01-24 06:18:35 +0200

    Update v.php

--- event(mvc)/v.php
+++ event(mvc)/v.php
@@ -1,16 +1,26 @@
-<?php
-class eventview
-{
-     function eventdet($obj)
-    {        
-        echo "name" . $obj->name ."<br>";
-        echo "code" . $obj->code ."<br>";
-        echo "description" . $obj->description ."<br>"; 
-        echo "location" . $obj->location ."<br>"; 
-        echo "isdeleted" . $obj->isdeleted ."<br>"; 
-        echo "createdat" . $obj->createdat ."<br>"; 
-        echo "updatedat" . $obj->updatedat ."<br>";         
-    }
-}
-
-?>
+<head>
+<meta charset="UTF-8">
+<title>Add Record Form</title>
+</head>
+<body>
+<form action="c.php" method="post">
+    <p>
+        <label for="name">event Name:</label>
+        <input type="text" name="name" id="name">
+    </p>
+    <p>
+        <label for="code">event code:</label>
+        <input type="text" name="code" id="code">
+    </p>
+    <p>
+        <label for="description">event description:</label>
+        <input type="text" name="description" id="description">
+    </p>
+    <p>
+        <label for="location">event location:</label>
+        <input type="text" name="location" id="location">
+    </p>
+    <input type="submit" value="Submit">
+</form>
+</body>
+</html>
