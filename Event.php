018e486207223022123d8a616165fca2b36a6d99
YehiaM11 <93736300+YehiaM11@users.noreply.github.com>
2022-01-24 22:40:57 +0200

    Update Event.php
    now you can see the event details

--- Event.php
+++ Event.php
@@ -1,9 +1,11 @@
 <head>
 <meta charset="UTF-8">
-<title>Add Record Form</title>
+<title>create event Form</title>
 </head>
 <body>
-<form action="./controllers/Events.php" method="post">
+
+<h1>create event Form</h1>
+<form action="c.php" method="post">
     <p>
         <label for="name">event Name:</label>
         <input type="text" name="name" id="name">
@@ -22,5 +24,33 @@
     </p>
     <input type="submit" value="Submit">
 </form>
+<br>
+<?php
+////viewing event rows
+  $con = mysqli_connect('localhost','root','','charityfunding');
+  $query = "SELECT * FROM event";
+$results=mysqli_query($con,$query);
+$row_count=mysqli_num_rows($results);
+echo '<h1>events</h1>';
+  echo "<table>";
+  echo '<th><p>id</p></th>';
+  echo '<th><p>name</p></th>';
+  echo '<th><p>code</p></th>';
+  echo '<th><p>description</p></th>';
+  echo '<th><p>location</p></th>';
+  echo '<th><p>isdeleted</p></th>';
+  echo '<th><p>createdat</p></th>';
+  echo '<th><p>updatedat</p></th>';
+while ($row = mysqli_fetch_array($results)) {
+    //output a row here
+    echo '<tr>';
+    echo '<td>'.($row['id']).'</td><td>'.($row['name']).'</td><td>'.($row['code']).'</td><td>'.($row['description']).'</td><td>'.($row['location']).'</td><td>'.($row['isdeleted']).'</td><td>'.($row['createdat']).'</td><td>'.($row['updatedat']).'</td><br>';
+    echo '</tr>';
+}
+echo "</table>";
+  $conn = NULL;
+?>
+<br><br>
+
 </body>
 </html>
