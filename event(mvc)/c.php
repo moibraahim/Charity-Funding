9b2f6a727dabe339080aa690e030750dddb29dae
YehiaM11 <93736300+YehiaM11@users.noreply.github.com>
2022-01-24 06:19:34 +0200

    Update c.php

--- event(mvc)/c.php
+++ event(mvc)/c.php
@@ -1,16 +1,19 @@
 <?php
-include_once ('m.php');
 include_once ("v.php");
-if(isset($_REQUEST["id"]))
-{
-    $eventobj= new event($_REQUEST["id"]);
-   // echo($eventobj->name);
-    $vv=new eventview();
-    $vv->eventdet($eventobj);
-}
-if($_REQUEST["command"]=="insert")
-{
-    $eventobj=new event(0);
-    $eventobj->insert($_REQUEST["name"],$_REQUEST["code"],$_REQUEST["description"],$_REQUEST["location"]);
+// Define variables and initialize with empty values
+$name = $code = $description =$location= "";
+// Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $input_name = trim($_POST["name"]);
+    $name = $input_name;
+    $input_code = trim($_POST["code"]);
+    $code = $input_code;
+    $input_description = trim($_POST["description"]);
+    $description = $input_description;   
+    $input_location = trim($_POST["location"]);
+    $location = $input_location;    
+    include_once 'm.php';
+    $creator = new event(0);
+    $creator->insert($name,$code,$description,$location);
 }
 ?>
