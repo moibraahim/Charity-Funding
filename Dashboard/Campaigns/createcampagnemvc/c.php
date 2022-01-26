612697480908091fa893c7f1f71d39942cdf365a
YehiaM11 <93736300+YehiaM11@users.noreply.github.com>
2022-01-26 06:31:20 +0200

    validation on inputs

--- Dashboard/Campaigns/createcampagnemvc/c.php
+++ Dashboard/Campaigns/createcampagnemvc/c.php
@@ -11,8 +11,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $code = $input_code;
     $input_description = trim($_POST["description"]);
     $description = $input_description;    
+      if (  empty($name) || empty($goalprice) || empty ( $code) || empty( $description))
+    {        
+        echo '<script type="text/javascript">alert("Please fill out all inputs");</script>';
+    }
+    elseif(preg_match("/^[a-zA-Z0-9]+$/", $name) == 1) {
+        echo '<script type="text/javascript">alert("enter a proper name");</script>';
+    }
+    elseif ($$goalprice < 0)
+    {
+        echo '<script type="text/javascript">alert("enter proper price");</script>';
+    }
+    else{
     include_once 'm.php';
     $creator = new campaign(0);
     $creator->insert($name,$goalprice,$code,$description);
+    }
 }
 ?>
